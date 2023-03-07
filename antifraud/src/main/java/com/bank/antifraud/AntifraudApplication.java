9d27870eb4d6ee20bf58e2684ea4b882a80af58e
rvd1cvl <mzilcov08@gmail.com>
2023-03-07 19:46:51 +0300

    удалил лишние аннотации

--- antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableEurekaClient
 public class AntifraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntifraudApplication.class, args);
