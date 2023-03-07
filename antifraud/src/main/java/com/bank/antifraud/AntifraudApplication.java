7958c6bad74674d2fe827ee154b27b0405bad65c
rvd1cvl <mzilcov08@gmail.com>
2023-03-07 13:48:30 +0300

    рабочий eureka client

--- antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
@@ -2,8 +2,12 @@ package com.bank.antifraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class AntifraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(AntifraudApplication.class, args);
